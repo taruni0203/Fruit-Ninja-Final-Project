58c61d406e61840be61c78c525b456ea2b394085
taruni0203 <67770771+taruni0203@users.noreply.github.com>
2020-08-23 10:50:47 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,16 @@
 var ninja;
 var AppleGroup, MelonGroup, StrawGroup, PeachGroup;
-var count,ground, gameOver,time;
+var count,ground, gameOver,time,star;
 
-var star;
 var score = 0;
 var pScore = 0;
 var visibility = 255;
 var chance = 0;
-var image1,image2,image3;
-var ninjaImg,star3, star4;
+var image1,image2,image3,ninjaImg,star3, star4;
+
 
 var blade = 0;
-
 var  gameState = "rules";
-
 //mode is different from gameState, specifies the level of game
 var mode = "ran";
 
@@ -44,7 +41,7 @@ function preload(){
 
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(windowWidth/2,windowHeight/2);
  
   AppleGroup = createGroup();
   StrawGroup = createGroup();
@@ -53,66 +50,66 @@ function setup() {
   BoomGroup = createGroup();
   NinjaGroup = createGroup();
 
-  ninja = createSprite(200,375,0.05,0.05);
+  ninja = createSprite(width/2,height-80,0.05,0.05);
  
   yesButton = createButton("Yes");
-  yesButton.position(400,350);
+  yesButton.position(width/2,height*3/4);
   yesButton.hide();
 
   startButton = createButton("Start");
-  startButton.position(380,300);
+  startButton.position(width/2-10,height*7/10);
   startButton.hide();
 
   resetButton = createButton("Reset");
-  resetButton.position(100,50);
+  resetButton.position(width/8,height/8);
   resetButton.hide();
 
-  gameOver = createSprite(400,120,20,20);
+  gameOver = createSprite(width/2,height/3,20,20);
   gameOver.addImage(gameOverImg);
   gameOver.visible = false;
-  power = createSprite(400,70,20,20);
+
+  power = createSprite(width/2,height/5,20,20);
   power.addImage(powerImg);
   power.scale = 0.6;
   power.visible = false;
 
-  image1 = createSprite(250,200,50,50);
+  image1 = createSprite(width/3,height/2,50,50);
   image1.addImage(ninjaImg);
   image1.scale = 0.1;
 
-  image2 = createSprite(400,200,50,50);
+  image2 = createSprite(width/2 + 20,height/2,50,50);
   image2.addImage(star3);
   image2.scale = 0.3;
 
-  image3 = createSprite(550,200,50,50);
+  image3 = createSprite(width*2/3 + 30,height/2,50,50);
   image3.addImage(star4);
   image3.scale = 0.14;
+  console.log(width);
 
-  x1 = createSprite(640,100,20,20);
+  x1 = createSprite(width*3/4 + 20,height/4,20,20);
   x1.addImage(xImg);
   x1.scale = 0.7;
 
-  x2 = createSprite(680,100,20,20);
+  x2 = createSprite(width*5/6 + 10,height/4,20,20);
   x2.addImage(xImg);
   x2.scale = 0.7;
 
-  x3 = createSprite(720,100,20,20);
+  x3 = createSprite(width*9/10 + 10,height/4,20,20);
   x3.addImage(xImg);
   x3.scale = 0.7;
 
-
   begButton = createButton("Beginner");
-  begButton.position(300,100);
+  begButton.position(width*3/8,height/4);
   begButton.hide();
 
   advButton = createButton("Advance");
-  advButton.position(500,100);
+  advButton.position(width*5/8,height/4);
   advButton.hide();
 
-  wall1 = createSprite(810,200,20,400);
-  wall2 = createSprite(-10,200,20,400);
+  wall1 = createSprite(width+10,height/2,20,height);
+  wall2 = createSprite(-10,height/2,20,height);
 
   ninja.velocityX = 25;
-
 }
 
 function draw() {
@@ -139,95 +136,101 @@ function draw() {
     ninja.x = mouseX;
   }
 
-if(gameState === "rules"){
-  yesButton.show();
-  textSize(25);
-  text("Rules: ",350,170);
-  text("The goal of this game is to slice the fruit",160,200);
-  text("To slice the fruit, press the space bar",160,230);
-  text("Beware of the falling bombs!",160,260);
-  text("Are you ready to become the next fruit ninja?",160,290);
-  yesButton.mousePressed(()=>{
-    gameState = "options";
-    yesButton.hide();
-  })
-}
+    if(gameState === "rules"){
+      yesButton.show();
+      textSize(25);
+      text("Rules: ",width/2-30,height*3/8-10);
+      text("The goal of this game is to slice the fruit",width/3,height*7/16);
+      text("To slice the fruit, press the space bar",width/3,height/2+10);
+      text("Beware of the falling bombs!",width/3,height*9/16+20);
+      text("Are you ready to become the next fruit ninja?",width/3,height*5/8+30);
+      yesButton.mousePressed(()=>{
+        gameState = "options";
+        yesButton.hide();
+      })
+      
+    }
 
-if(gameState === "options"){
-  textSize(30);
-  text("Choose Your Ninja Blade",250,100);
-  image1.visible = true;
-  image2.visible = true;
-  image3.visible = true;
+    if(gameState === "options"){
+      textSize(30);
+      text("Choose Your Ninja Blade",width*5/16,height/4);
+      image1.visible = true;
+      image2.visible = true;
+      image3.visible = true;
 
-  if(mousePressedOver(image1)){
-    blade = 1;
-    startButton.show();
-  } 
-  if (mousePressedOver(image2)){
-    blade = 2;
-    startButton.show();
-  } 
-  if(mousePressedOver(image3)){
-    blade = 3;
-    startButton.show();
-  }
+      if(mousePressedOver(image1)){
+        blade = 1;
+        startButton.show();
+      } 
+      if (mousePressedOver(image2)){
+        blade = 2;
+        startButton.show();
+      } 
+      if(mousePressedOver(image3)){
+        blade = 3;
+        startButton.show();
+      }
 
-  startButton.mousePressed(()=>{
-    gameState = "options2";
-    startButton.hide();
-  })
-}
+      startButton.mousePressed(()=>{
+        gameState = "options2";
+        startButton.hide();
+      })
+    }
 
-if(mousePressedOver(image1)){
-  ninja.scale = 0.05;
-  ninja.addImage(ninjaImg);
-}else if(mousePressedOver(image2)){
-  ninja.scale = 0.15;
-  ninja.addImage(star3);
-}else if(mousePressedOver(image3)){
-  ninja.scale = 0.08;
-  ninja.addImage(star4);
-}
+    if(mousePressedOver(image1) || touches.length>0){
+      ninja.scale = 0.05;
+      ninja.addImage(ninjaImg);
+      touches=[];
+    }else if(mousePressedOver(image2) || touches.length>0){
+      ninja.scale = 0.15;
+      ninja.addImage(star3);
+      touches=[];
+    }else if(mousePressedOver(image3) || touches.length>0){
+      ninja.scale = 0.08;
+      ninja.addImage(star4);
+      touches=[];
+    }
 
-if(gameState === "options2"){
-  begButton.show();
-  advButton.show();
+    if(gameState === "options2"){
+      begButton.show();
+      advButton.show();
 
-  begButton.mousePressed(()=>{
-    mode = "beg";
-    gameState = "play";
-    begButton.hide();
-    
-  })
+      begButton.mousePressed(()=>{
+        mode = "beg";
+        gameState = "play";
+        begButton.hide();
+        
+      })
 
-  advButton.mousePressed(()=>{
-    mode = "adv";
-    gameState = "play";
-    advButton.hide();
-  })
-}
+      advButton.mousePressed(()=>{
+        mode = "adv";
+        gameState = "play";
+        advButton.hide();
+      })
+    }
+
     if(gameState === "play"){
       power.visible = false; 
       gameOver.visible = false;
       textSize(20);
-      text("Score: "+ score,630,60);
+      text("Score: "+ score,width*3/4 + 30,height/5-15);
         spawnApples();
         spawnStraws();
         spawnMelons();
         spawnPeaches();
         spawnBooms();
-        if(keyWentDown("space")){
+        if(keyWentDown("space") || touches.length>0){
           //Creates a star to slice the fruit.
-          createStar();    
+          createStar(); 
+          touches = [];   
         }
     
-        if(frameCount %Math.round(random(900,1500)) === 0){
+        if(frameCount %Math.round(random(800,1400)) === 0){
           clockSound.play();
           time = 10;
           gameState = "powerPlay";
         }
-        if(score >0 && score % 50 === 0){
+        if(score >0 && score % 500 === 0){
           time = 5
           gameState = "autoMode";
         }
@@ -239,17 +242,16 @@ if(gameState === "options2"){
     /*AutoMode is a way of rewarding the player by giving them 5 seconds 
     to relax and automatically score points*/
       textSize(30);
-      text("Auto Mode!",300,50);
-      
+      text("Auto Mode!",width*3/8,height/5);
       time = time - 0.03;
       textSize(20);
-      text("Time Left 0:0" + Math.round(time),40,60);
+      text("Time Left 0:0" + Math.round(time),width/20,height/5-15);
       if(time < 0){
         NinjaGroup.destroyEach();
         gameState = "play";
       }
     
-      text("Score: "+ score,630,60);
+      text("Score: "+ score, width*3/4 + 30,height/5-15);
     
       ninja.bounceOff(wall1);
       ninja.bounceOff(wall2);
@@ -267,11 +269,10 @@ if(gameState === "options2"){
       //in powerplay, players can earn double points and bombs will not have an effect
       power.visible = true; 
       textSize(20);
-      text("Power Score: "+ pScore,620,60);
+      text("Power Score: "+ pScore, width*3/4 + 20,height/5-15);
       
       time = time - 0.03;
-      text("Time Left 0:0" + Math.round(time),40,60);
-    
+      text("Time Left 0:0" + Math.round(time), width/20,height/5-15);
       if(time < 0){
         gameState = "play";
       }
@@ -282,8 +283,9 @@ if(gameState === "options2"){
       spawnPeaches();
       spawnBooms();
     
-      if(keyWentDown("space")){
+      if(keyWentDown("space") || touches.length>0){
         createStar();
+        touches = [];
       }
     
       fruitCut(); 
@@ -291,7 +293,6 @@ if(gameState === "options2"){
     
     if(gameState === "end"){
       power.visible = false;
-
       //you get three chances before completely dying
       if(chance === 3){
         gameOver.visible = true;
@@ -300,7 +301,6 @@ if(gameState === "options2"){
         resetButton.show();
       }
     
-      
       AppleGroup.destroyEach();
       PeachGroup.destroyEach();
       MelonGroup.destroyEach();
@@ -310,12 +310,12 @@ if(gameState === "options2"){
       fill("white");
       stroke("white");
       if(pScore > 0){
-        text("Score: " + score,340,260);
-        text("Power Score: "+ pScore,300,300);
-        text("Total Score: "+ (score + pScore),300,340);
+        text("Score: " + score,width*7/16,height*11/16 - 30);
+        text("Power Score: "+ pScore,width/2,height*3/4 - 20);
+        text("Total Score: "+ (score + pScore),width/2,height*13/16 - 10);
       }else{
-        text("Score: " + score,340,280);
-        text("Total Score: "+ score,300,320);
+        text("Score: " + score, width*7/16,height*3/4-20);
+        text("Total Score: "+ score,width*7/16,height*3/4+20);
       }
     
       resetButton.mousePressed(()=>{
@@ -335,16 +335,19 @@ if(gameState === "options2"){
   drawSprites();
 }
 
-//These spawn functions are to create the various fruits and show on screen
+/*These spawn functions are to create the various fruits and show on screen according to 
+frameCount and random places*/
 
 function spawnApples(){
   if(frameCount %25 === 0){
-    var apple = createSprite(random(0,800),random(-80,0),20,20);
+    var apple = createSprite(random(0,width),random(-80,0),20,20);
     apple.addImage(appleImg);
     if(mode === "beg"){
       apple.velocityY = 8 + 3*(frameCount/500);
+      apple.lifetime = height/(8 + 3*(frameCount/500));
     }else if(mode === "adv"){
-      apple.velocityY = 13 + 3*(frameCount/500);
+      apple.velocityY = 11 + 3*(frameCount/500);
+      apple.lifetime = height/(11 + 3*(frameCount/500));
     }
     apple.scale = 0.6;
     AppleGroup.add(apple);
@@ -352,12 +355,14 @@ function spawnApples(){
 }
 function spawnStraws(){
   if(frameCount %25 === 0){
-    var straw = createSprite(random(0,800),random(-80,0),20,20);
+    var straw = createSprite(random(0,width),random(-80,0),20,20);
     straw.addImage(strawImg);
     if(mode === "beg"){
       straw.velocityY = 8 + 3*(frameCount/500);
+      straw.lifetime = height/(8 + 3*(frameCount/500));
     }else if(mode === "adv"){
-      straw.velocityY = 13 + 3*(frameCount/500);
+      straw.velocityY = 11 + 3*(frameCount/500);
+      straw.lifetime = height/(11 + 3*(frameCount/500));
     }
     straw.scale = 0.6;
     StrawGroup.add(straw);
@@ -365,12 +370,14 @@ function spawnStraws(){
 }
 function spawnMelons(){
   if(frameCount %25 === 0){
-    var melon = createSprite(random(0,800),random(-80,0),20,20);
+    var melon = createSprite(random(0,width),random(-80,0),20,20);
     melon.addImage(melonImg);
     if(mode === "beg"){
       melon.velocityY = 8 + 3*(frameCount/500);
+      melon.lifetime = height/(8 + 3*(frameCount/500));
     }else if(mode === "adv"){
-      melon.velocityY = 13 + 3*(frameCount/500);
+      melon.velocityY = 11 + 3*(frameCount/500);
+      melon.lifetime = height/(11 + 3*(frameCount/500));
     }
     melon.scale = 0.6;
     MelonGroup.add(melon);
@@ -378,12 +385,14 @@ function spawnMelons(){
 }
 function spawnPeaches(){
   if(frameCount %25 === 0){
-    var peach = createSprite(random(0,800),random(-80,0),20,20);
+    var peach = createSprite(random(0,width),random(-80,0),20,20);
     peach.addImage(peachImg);
     if(mode === "beg"){
       peach.velocityY = 8 + 3*(frameCount/500);
+      peach.lifetime = height/(8 + 3*(frameCount/500));
     }else if(mode === "adv"){
-      peach.velocityY = 13 + 3*(frameCount/500);
+      peach.velocityY = 11 + 3*(frameCount/500);
+      peach.lifetime = height/(11 + 3*(frameCount/500));
     }
     peach.scale = 0.6;
     PeachGroup.add(peach);
@@ -391,23 +400,23 @@ function spawnPeaches(){
 }
 function spawnBooms(){
   if(frameCount %60 === 0){
-    var boom = createSprite(random(0,800),random(-80,0),20,20);
+    var boom = createSprite(random(0,width),random(-80,0),20,20);
     boom.addImage(boomImg);
     if(mode === "beg"){
       boom.velocityY = 8 + 3*(frameCount/500);
+      boom.lifetime = height/(8 + 3*(frameCount/500));
     }else if(mode === "adv"){
-      boom.velocityY = 13 + 3*(frameCount/500);
+      boom.velocityY = 11 + 3*(frameCount/500);
+      boom.lifetime = height/(11 + 3*(frameCount/500));
     }
     boom.rotationSpeed = random(-0.5,0.5);
     boom.scale = 0.8;
-    boom.lifetime = 400/(8 + 3*(frameCount/1000));
     BoomGroup.add(boom);
   }
 }
 
-
 function createStar(){
-  star = createSprite(ninja.x,380,20,20);
+  star = createSprite(ninja.x,height-30,20,20);
   star.velocityY = -7;
   star.rotationSpeed = 20;
   //star.addImage(ninjaImg);
@@ -428,6 +437,9 @@ function createStar(){
 }
 
 function fruitCut(){
+  /*the purpose of each of these for loops is to point to each individual fruit, "slice"
+  the fruit by creating a sprite for each slice and destroying the whole fruit, and 
+  increase the score based on the mode and gameState*/
   for(var a = 0; a<AppleGroup.length; a++){
     var appleTemp = AppleGroup.get(a);
     for(var n = 0; n<NinjaGroup.length; n++){
@@ -452,7 +464,7 @@ function fruitCut(){
         if(mode === "beg"){
           appleS.velocityY =  random(6 + 3*(frameCount/500),10 + 3*(frameCount/500));
         }else if(mode === "adv"){
-          appleS.velocityY = random(11 + 3*(frameCount/500),15 + 3*(frameCount/500));
+          appleS.velocityY = random(9 + 3*(frameCount/500),13 + 3*(frameCount/500));
         }
         appleS.rotationSpeed = random(-3,3)
         appleS2 = createSprite(appleTemp.x + 10,appleTemp.y,20,20);
@@ -461,18 +473,15 @@ function fruitCut(){
         if(mode === "beg"){
           appleS2.velocityY =  random(6 + 3*(frameCount/500),10 + 3*(frameCount/500));
         }else if(mode === "adv"){
-          appleS2.velocityY = random(11 + 3*(frameCount/500),15 + 3*(frameCount/500));
+          appleS2.velocityY = random(9 + 3*(frameCount/500),13 + 3*(frameCount/500));
         }
         appleS2.rotationSpeed = random(-3,3);
-        if(appleS.y>400){
+        if(appleS.y>height){
           appleS.destroy();
         }
-        if(appleS2.y>400){
+        if(appleS2.y>height){
           appleS2.destroy();
         }
-      }
-      if(appleTemp.y>400){
-        appleTemp.destroy();
       }
     }
   }
@@ -499,7 +508,7 @@ function fruitCut(){
         if(mode === "beg"){
           strawS.velocityY =  random(6 + 3*(frameCount/500),10 + 3*(frameCount/500));
         }else if(mode === "adv"){
-          strawS.velocityY = random(11 + 3*(frameCount/500),15 + 3*(frameCount/500));
+          strawS.velocityY = random(9 + 3*(frameCount/500),13 + 3*(frameCount/500));
         }
         strawS.rotationSpeed = random(-3,3)
         strawS2 = createSprite(strawTemp.x + 10,strawTemp.y,20,20);
@@ -508,22 +517,19 @@ function fruitCut(){
         if(mode === "beg"){
           strawS2.velocityY =  random(6 + 3*(frameCount/500),10 + 3*(frameCount/500));
         }else if(mode === "adv"){
-          strawS2.velocityY = random(11 + 3*(frameCount/500),15 + 3*(frameCount/500));
+          strawS2.velocityY = random(9 + 3*(frameCount/500),13 + 3*(frameCount/500));
         }
         strawS2.rotationSpeed = random(-3,3);
-        if(strawS.y>400){
+        if(strawS.y>height){
           strawS.destroy();
         }
-        if(strawS2.y>400){
+        if(strawS2.y>height){
           strawS2.destroy();
         }
       }
-      if(strawTemp.y>400){
-        strawTemp.destroy();
-      }
+
     }
   }
-  
   for(var m = 0; m<MelonGroup.length; m++){
     var melonTemp = MelonGroup.get(m);
       for(var n = 0; n<NinjaGroup.length; n++){
@@ -539,7 +545,6 @@ function fruitCut(){
         }if(gameState === "powerPlay" && mode === "beg"){
           pScore = pScore + 10;
         }
-
         melonTemp.destroy();
         ninjaTemp.destroy();
         melonS = createSprite(melonTemp.x-10,melonTemp.y,20,20);
@@ -548,7 +553,7 @@ function fruitCut(){
         if(mode === "beg"){
           melonS.velocityY =  random(6 + 3*(frameCount/500),10 + 3*(frameCount/500));
         }else if(mode === "adv"){
-          melonS.velocityY = random(11 + 3*(frameCount/500),15 + 3*(frameCount/500));
+          melonS.velocityY = random(9 + 3*(frameCount/500),13 + 3*(frameCount/500));
         }
         melonS.rotationSpeed = random(-3,3)
         melonS2 = createSprite(melonTemp.x + 10,melonTemp.y,20,20);
@@ -557,22 +562,19 @@ function fruitCut(){
         if(mode === "beg"){
           melonS2.velocityY =  random(6 + 3*(frameCount/500),10 + 3*(frameCount/500));
         }else if(mode === "adv"){
-          melonS2.velocityY = random(11 + 3*(frameCount/500),15 + 3*(frameCount/500));
+          melonS2.velocityY = random(9 + 3*(frameCount/500),13 + 3*(frameCount/500));
         }
         melonS2.rotationSpeed = random(-3,3);
-        if(melonS.y>400){
+        if(melonS.y>height){
           melonS.destroy();
         }
-        if(melonS2.y>400){
+        if(melonS2.y>height){
           melonS2.destroy();
         }
       }
-      if(melonTemp.y>400){
-        melonTemp.destroy();
-      }
+
     }
   }
-
   for(var p = 0; p<PeachGroup.length; p++){
     var peachTemp = PeachGroup.get(p);
     for(var n = 0; n<NinjaGroup.length; n++){
@@ -596,7 +598,7 @@ function fruitCut(){
         if(mode === "beg"){
           peachS.velocityY =  random(6 + 3*(frameCount/500),10 + 3*(frameCount/500));
         }else if(mode === "adv"){
-          peachS.velocityY = random(11 + 3*(frameCount/500),15 + 3*(frameCount/500));
+          peachS.velocityY = random(9 + 3*(frameCount/500),13 + 3*(frameCount/500));
         }
         peachS.rotationSpeed = random(-3,3)
         peachS2 = createSprite(peachTemp.x + 10,peachTemp.y,20,20);
@@ -605,18 +607,15 @@ function fruitCut(){
         if(mode === "beg"){
           peachS2.velocityY =  random(6 + 3*(frameCount/500),10 + 3*(frameCount/500));
         }else if(mode === "adv"){
-          peachS2.velocityY = random(11 + 3*(frameCount/500),15 + 3*(frameCount/500));
+          peachS2.velocityY = random(9 + 3*(frameCount/500),13 + 3*(frameCount/500));
         }
         peachS2.rotationSpeed = random(-3,3);
-        if(peachS.y>400){
+        if(peachS.y>height){
           peachS.destroy();
         }
-        if(peachS2.y>400){
+        if(peachS2.y>height){
           peachS2.destroy();
         }
-      }
-      if(peachTemp.y>400){
-        peachTemp.destroy();
       }
     }
   }
@@ -627,9 +626,9 @@ function fruitCut(){
       if(ninjaTemp.isTouching(boomTemp) && gameState=== "play"){
         boomSound.play();
         gameState = "end";
+        //chance is a variable used to track the strikeouts
         chance = chance + 1;
       }
     }
   }
 }
-
